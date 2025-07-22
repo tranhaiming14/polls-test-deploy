f68d185d1646d1f52e0f2f75c790edb624ad538b
tranhaiming14 <tnam37567@gmail.com>
2025-07-22 10:52:51 +0700

    Fixed

--- create_superuser.py
+++ create_superuser.py
@@ -2,7 +2,7 @@
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 django.setup()
 
 from django.contrib.auth import get_user_model
