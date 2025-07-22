d5ffe2c1efee70b05315fc685bacdd8bf3fc82d5
tranhaiming14 <tnam37567@gmail.com>
2025-07-22 10:43:55 +0700

    Using postgresql instead

---
+++ create_superuser.py
@@ -0,0 +1,20 @@
+# create_superuser.py
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project.settings")
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username=os.environ["DJANGO_ADMIN_USER"]).exists():
+    User.objects.create_superuser(
+        os.environ["DJANGO_ADMIN_USER"],
+        os.environ["DJANGO_ADMIN_EMAIL"],
+        os.environ["DJANGO_ADMIN_PASSWORD"]
+    )
+    print("✅ Superuser created.")
+else:
+    print("Superuser already exists.")
--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,11 +74,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
